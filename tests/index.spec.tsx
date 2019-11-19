0fc5da82912acb8256a0044655ba2094602cf59f
XueYou <xueyoucd@gmail.com>
2019-11-19 11:34:09 +0800

    - 修复`react`与`react-dom`版本不一致导致的问题

--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render } from "@testing-library/react";
 import Affix from "../src";
 
 describe("Affix", () => {
